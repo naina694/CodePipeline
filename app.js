2a94222c7f6880edf5b21d3b1df339e0360d49cc
naina694 <17bt030430naina@gmail.com>
2026-06-24 13:23:36 +0530

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var express = require('express');
+const express = require('express');
 
-// create a new express server
-var app = express();
+const app = express();
+const port = process.env.PORT||3000;
+app.get('/',(req,res)=>{
+    res.send('This is the First Version')
+})
 
-var port=process.env.PORT || 8000;
-
-// serve the files out of ./public1 as our main files
-app.use(express.static(__dirname + '/public1'));
-
-app.listen(port,function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + port);
+app.listen(port, () => {
+    console.log(Server is running on http://localhost:${port});
 });
